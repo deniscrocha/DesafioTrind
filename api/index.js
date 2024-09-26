ee92edfca08eeff6272188972793cac65f8419b5
Dênis Rocha <denischarocha@gmail.com>
2024-09-26 15:20:24 -0300

    Fixed API to work with new routes and new structure

--- api/index.js
+++ api/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const knex = require("./database/db");
-const studentsRouter = require("./routes/studentsRoute");
 const { createDatabase } = require("./database/tableCreation");
-const addressRouter = require("./routes/addressRoute");
-const coursesRouter = require("./routes/coursesRoute");
+const studentsRouter = require("./controllers/studentsController");
+const linkRouter = require("./controllers/studentsCourseController");
+const coursesRouter = require("./controllers/coursesController");
+
 const app = express();
 const port = 3000;
 knex.schema.hasTable('students').then(function (exists) {
 	if (!exists) {
 		createDatabase();
 	}
-  });
+});
 
 app.use("/students/", studentsRouter);
 app.use("/courses/", coursesRouter);
-app.use("/address/", addressRouter);
+app.use("/links/", linkRouter);
 
 app.listen(port, () =>{
 	console.log("Listen on port: ", port);
